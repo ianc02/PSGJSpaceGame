381f6b2f6cdc9ac00a8a12e071589fbed8195113
Ian Campbell <campbellni@hendrix.edu>
2025-01-25 21:16:39 -0600

    Included FMOD

---
+++ PSGJSpace/Assets/Scripts/Attributions.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attributions : MonoBehaviour
+{
+    /*
+     * https://signal.vercel.app/edit - Software used to create music for game
+     * 
+     */
+}
